c2cbce0c131b58d30a5bd6b3ef8450f82626552c
DevDoctor <84864508+Dev-Doctor@users.noreply.github.com>
2025-05-05 11:51:38 +0200

    index revamp

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,31 @@
+$(document).ready(function () {
+    const trigger = $('#collapseTrigger');
+    const menu = $('#collapseExample');
+
+    // Show menu on hover in
+    trigger.hover(
+        function () {
+            menu.stop(true, true).fadeIn(200);
+        },
+        function () {
+            // Delay fade out to allow hover into menu
+            setTimeout(function () {
+                if (!menu.is(':hover')) {
+                    menu.stop(true, true).fadeOut(200);
+                }
+            }, 100);
+        }
+    );
+
+    // Also hide menu when cursor leaves the menu itself
+    menu.hover(
+        function () { },
+        function () {
+            menu.stop(true, true).fadeOut(200);
+        }
+    );
+});
+
 window.onload = function () {
     $('.moai').on("mouseover", function () {
         var audio = document.getElementById("skibidi");
