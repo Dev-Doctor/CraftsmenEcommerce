6d0b2c09edd573581104614fe62a1e98ed371865
lardo <lardo.mango.pyxxa@gmail.com>
2025-05-05 21:09:10 +0200

    getproduct

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../db');
 
 router.get('/', async function (req, res) {
-    res.json({
-        test: "gaygagaygagay"
-    }
-    );
-    res.statusCode('418');
+   // const client = await pool.connect()
+    const {rows} = await pool.query('SELECT * from products WHERE id = $1 ',[1])
+    res.json(rows[0]);
+   // client.release()
 });
 
 module.exports = router;
